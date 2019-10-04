fe69b2fe6b337eaa15b2020ace4595706866e5a9
aaronbrick <abrick@ccsf.edu>
2019-10-04 09:49:50 -0700

    added first test

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
 # in this file locate the test cases for our classes.
 
+
+# create a space with 100 tasks and find them
+from space import Space
+s = Space()
+for i in range(100):
+ s.add()
+print(s.find())
+
+
